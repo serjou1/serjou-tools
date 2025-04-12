f777e55873198fb394219c9452c1942bb9d488b0
serjou1 <sergnik97@gmail.com>
2025-04-12 22:42:03 +0300

    added new util

--- src/utils.ts
+++ src/utils.ts
@@ -1,3 +1,5 @@
+import * as d3 from "d3";
+
 export const sleep = async (ms: number) => {
     return new Promise((resolve) => {
         setInterval(resolve, ms);
@@ -6,4 +8,20 @@ export const sleep = async (ms: number) => {
 
 export function onlyUnique(value: any, index: number, array: any[]) {
     return array.indexOf(value) === index;
-}
+};
+
+export const toStep = (value: number, step: number | string) => {
+    const stepNumber = Number(step);
+    if (stepNumber > 1) {
+        return roundToStep(value, stepNumber);
+    }
+
+    const pr = d3.precisionFixed(stepNumber);
+    const f = d3.format(`.${pr}f`);
+
+    return Number(f(value));
+};
+
+const roundToStep = (value: number, step: number) => {
+    return Math.round(value / step) * step;
+};
